804b42749fb1c9a77d4b8b620b907b9b2682b09f
sibo <roi.siboni@msmail.ariel.ac.il>
2024-10-08 11:34:46 +0300

    lemur

--- src/mst/BoruvkaMST.hpp
+++ src/mst/BoruvkaMST.hpp
@@ -12,5 +12,4 @@ private:
     // Helper functions for Borůvka's algorithm
     int find(std::vector<int> &parent, int i);
     void unite(std::vector<int> &parent, std::vector<int> &rank, int x, int y);
-    void findCheapest(const Graph &graph, std::vector<int> &cheapest, const std::vector<int> &parent);
 };
